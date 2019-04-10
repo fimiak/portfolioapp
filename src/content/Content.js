7a7c0933b23ec60ac1742b4f4d2c60ab3681d107
Tyler James Greve <tylerjamesgreve@gmail.com>
2019-04-10 13:29:15 -0700

    Added new content for recycle, some hover effects.

--- src/content/Content.js
+++ src/content/Content.js
@@ -6,12 +6,13 @@ function Content() {
   return (
     <div className="content">
       <div className="topmargin" />
-      <Project bg="bg-one" data={data[0]} />
-      <Project bg="bg-two" data={data[2]} />
-      <Project bg="bg-three" data={data[1]} />
-      <Project bg="bg-four" data={data[3]} />
-      <Project bg="bg-five" data={data[4]} />
-      <Project bg="bg-six" data={data[5]} />
+      <Project data={data[0]} />
+      <Project data={data[1]} />
+      <Project data={data[2]} />
+      <Project data={data[3]} />
+      <Project data={data[4]} />
+      <Project data={data[5]} />
+      <Project data={data[6]} />
       <div className="history">
         <h2>Education</h2>
         <p>Self-taught - Web Development</p>
