410b0eeb33dc19e5c1ea387dec784a3a45180258
Tyler James Greve <fimiak@gmail.com>
2019-10-29 16:30:17 -0700

    Added left graphic

--- src/pages/Bio.js
+++ src/pages/Bio.js
@@ -3,22 +3,25 @@ import React from 'react';
 function Bio() {
   return (
     <div className="intro" id="about">
-      <p>
-        I enjoy coding mobile and desktop React-based websites and applications. I am always looking to discover new
-        solutions and to create optimal experiences when working in an office or by remote communication.
-      </p>
-      <div className="skills">
-        <ul>
-          <li>
-            <h3>Core Tools</h3>
-          </li>
-          <li>HTML</li>
-          <li>CSS / SASS</li>
-          <li>Javascript / ES6</li>
-          <li>React / Redux / Gatsby</li>
-          <li>Wordpress / Shopify / Marketo</li>
-          <li>Git / Github / Testing / Design /Adobe CC</li>
-        </ul>
+      <div className="bio">
+        <h3>What I Do</h3>
+        <p>
+          I enjoy coding mobile and desktop React-based websites and applications. I am always looking to discover new
+          solutions and to create optimal experiences when working in an office or by remote communication.
+        </p>
+        <div className="skills">
+          <ul>
+            <li>
+              <h3>Core Tools</h3>
+            </li>
+            <li>HTML</li>
+            <li>CSS / SASS</li>
+            <li>Javascript / ES6</li>
+            <li>React / Redux / Gatsby</li>
+            <li>Wordpress / Shopify / Marketo</li>
+            <li>Git / Github / Testing / Design /Adobe CC</li>
+          </ul>
+        </div>
       </div>
     </div>
   );
